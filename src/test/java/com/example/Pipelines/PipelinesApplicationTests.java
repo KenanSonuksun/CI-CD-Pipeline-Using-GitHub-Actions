f0bf23ae2bca73f306f79b8ddcecea2ecc0a01d0
Kenan <ksonuksun@gmail.com>
2023-04-09 23:36:09 +0300

    code

---
+++ src/test/java/com/example/Pipelines/PipelinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pipelines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
